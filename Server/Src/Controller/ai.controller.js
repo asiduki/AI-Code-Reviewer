a5340fc1015d6a6ade177a57d2b19c9081409f35
asiduki <147263246+asiduki@users.noreply.github.com>
2025-07-27 21:02:23 +0530

    work on backend

---
+++ Server/Src/Controller/ai.controller.js
@@ -0,0 +1,16 @@
+import express from "express";
+import generateContent from "../Service/ai.service"; 
+
+export const codereviewer = async (req , res)=> {
+    const prompt = req.body.prompt;
+    if(!prompt) return res.status(400).json({message:"prompt is required"});
+    else{
+        try{
+            const airesponse = await generateContent(prompt);
+            res.send(airesponse) ;
+        }
+        catch(error){
+            res.status(500).json({message:"Internal server error"});
+        }
+    }
+}
--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const airouting = require("./Src/Routing/ai.rotes");
 
-app.use()
+
+app.use(cors());
+app.use(express.json());
+app.use("/ai", airouting );
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , ()=>{
+    console.log(`server is running on ${PORT}`);
+})
