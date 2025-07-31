4fe6f6597270802a2ca5e87a31a98c513ba5cf46
asiduki <147263246+asiduki@users.noreply.github.com>
2025-08-01 00:16:53 +0530

    change system instruction

--- Server/Src/Service/ai.service.js
+++ Server/Src/Service/ai.service.js
@@ -4,10 +4,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const ai = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 
 const model = ai.getGenerativeModel({ model: "gemini-2.5-flash" ,systemInstruction:`You are an AI code correction assistant. Your only task is to analyze the provided code, fix any syntax errors or logical bugs, and provide the corrected version. 
-
-Your response MUST be in two parts:
-1.  First, the complete, corrected code inside a code block.
-2.  Second, a brief, bulleted list explaining the changes you made.`});
+Provide the complete corrected code only.`});
 
 async function generateContent(prompt) {
   const result = await model.generateContent(prompt);
